be744cd7e29f02a507b32947cbd28180c0813283
Annick SOME <annicksome26@gmail.com>
2020-04-23 22:52:37 +0000

    Initial commit

---
+++ app/src/main/java/bf/spea/recycler/MainActivity.java
@@ -0,0 +1,33 @@
+package bf.spea.recycler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    Adapter adapter;
+    ArrayList<String> items;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        items = new ArrayList<>();
+        items.add("UML");
+        items.add("Marketing");
+        items.add("Anglais");
+        items.add("Technique de Compilation");
+        items.add("Statistique");
+        items.add("RO");
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new Adapter(this,items);
+        recyclerView.setAdapter(adapter);
+
+    }
+}
